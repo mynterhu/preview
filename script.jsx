aec351e1eb99154b9084ba233325d62437ae6dc8
mynter <andorsgm@gmail.com>
2021-06-26 17:03:36 +0200

    dv3

--- script.jsx
+++ script.jsx
@@ -1,9 +1,17 @@
 const text = ("Some quick example text to build on the card title and make up the bulk of the card's content.");
 const divCard = <div className={'card'} style={{ width: '18rem' }}><img srcSet={''} className={'card-img-top'}></img><div className={'card-body'}><p className={'card-text'}>{text}</p></div></div>;
 
-    function Card(text) {
-    return (
-        {divCard}
-    );};
-
-    ReactDOM.render(<Card />, document.getElementById("root"));
+ReactDOM.render(
+    <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+            </Card.Text>
+            <Button variant="primary">{text}</Button>
+        </Card.Body>
+    </Card>,
+    document.getElementById("root")
+);
