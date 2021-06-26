94d51fd84e317b9b5693cf10c330d7e3b9f26df0
mynter <andorsgm@gmail.com>
2021-06-26 18:42:29 +0200

    c

--- script.jsx
+++ script.jsx
@@ -1,4 +1,4 @@
-import { Card , CardDeck , CardGroup , CardColumns } from "https://unpkg.com/react-bootstrap@next/";
+/*import { Card , CardDeck , CardGroup , CardColumns } from "https://unpkg.com/react-bootstrap@next/";
 
 const text = ("Some quick example text to build on the card title and make up the bulk of the card's content.");
 function divCard() {
@@ -16,4 +16,20 @@ function divCard() {
 }
 
 ReactDOM.render(<divCard />, document.getElementById("root")
-);
+);*/
+
+var divCard = React.createClass({
+    render: function() {
+      return (
+  
+        <div className="card" style={{width: '18rem'}}>
+          <img src className="card-img-top" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the care and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      );
+    }
+  });
