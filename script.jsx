26af97d97203719a1882233c63ff509777f23b84
mynter <andorsgm@gmail.com>
2021-06-26 17:06:59 +0200

    dl8

--- script.jsx
+++ script.jsx
@@ -1,6 +1,4 @@
 const text = ("Some quick example text to build on the card title and make up the bulk of the card's content.");
-const divCard = <div className={'card'} style={{ width: '18rem' }}><img srcSet={''} className={'card-img-top'}></img><div className={'card-body'}><p className={'card-text'}>{text}</p></div></div>;
-
 ReactDOM.render(
     <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src="holder.js/100px180" />
